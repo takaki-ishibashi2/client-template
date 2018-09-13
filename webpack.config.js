860b3cc3530c6ef4bd0fb31d7960f003054e1d3d
takaki-ishibashi2 <>
2018-09-13 16:20:19 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/public/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js(x)$/,
+	use:[{
+	  loader: 'babel-loader',
+	}],
+	exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+}
