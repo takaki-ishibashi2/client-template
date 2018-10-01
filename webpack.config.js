e0bc40f044aa89ab64328552468daff5b41fa116
takaki-ishibashi2 <>
2018-10-01 16:49:35 +0900

    Separate for build npm command to prod/dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/public/index.jsx',
   output: {
     path: path.resolve(__dirname, 'build'),
